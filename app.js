6219a0f90e06700f524a2d0d08bd84a051e36a0b
Mohamed-S-Zaina <89095682+Mohamed-S-Zaina@users.noreply.github.com>
2023-01-22 14:59:53 +0200

    Setting Security HTTP Headers by (helmet) npm

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { globalError } = require('./controllers/errorController');
 const userRoute = require('./routes/userRoutes');
 const tourRoute = require('./routes/tourRoutes');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -12,7 +13,7 @@ const app = express();
     from making too many requests to our API
     and that will then help us preventing attacks
     like denial of service, or brute force attacks.
-  */
+*/
 const expressRateLimit = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
@@ -22,23 +23,38 @@ const expressRateLimit = rateLimit({
     'Too many accounts created from this IP, please try again after an 15 min',
 });
 
-// Middleware
+// Global Middleware
+
+// Setting Security HTTP Headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from the same IP
 app.use('/api', expressRateLimit);
-app.use(express.json());
+
+// Body parser, reading data from from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
+
 // Routes
 //Tours Routes
 app.use('/api/v1/tours', tourRoute);
+
 // User Routes
 app.use('/api/v1/users', userRoute);
+
 // to trigger the Error middleware
 app.all('*', (req, res, next) => {
   const originalUrl = req.originalUrl;
   next(new AppError(404, `Can't find ${originalUrl} on this server!`)); //when put a value inside the next() then the next will pass all the middleware until it reach the global error middleware
 });
+
 // Error middleware for Errors
 app.use(globalError);
 module.exports = app;
