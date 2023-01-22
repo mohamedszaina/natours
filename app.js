dd421c21a5329171015b96400891976cfde57864
Mohamed-S-Zaina <89095682+Mohamed-S-Zaina@users.noreply.github.com>
2023-01-22 14:33:55 +0200

    implementing expressRateLimit for too many req

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ const AppError = require('./utils/appError');
 const { globalError } = require('./controllers/errorController');
 const userRoute = require('./routes/userRoutes');
 const tourRoute = require('./routes/tourRoutes');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+
+/*  In order to prevent the same IP
+    from making too many requests to our API
+    and that will then help us preventing attacks
+    like denial of service, or brute force attacks.
+  */
+const expressRateLimit = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message:
+    'Too many accounts created from this IP, please try again after an 15 min',
+});
 
 // Middleware
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
 }
+app.use('/api', expressRateLimit);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 // Routes
